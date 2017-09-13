f22305b221301e8fba89c4d4f5c86bcc474b2e0c
Nathaniel Corley <nscorley@gmail.com>
2017-09-12 22:43:14 -0400

    Updating chess information

--- client/app/components/App.jsx
+++ client/app/components/App.jsx
@@ -26,7 +26,6 @@ import CHESS_SRC from '../assets/images/chess-icon.png';
 import RESUME from '../assets/pdfs/nathaniel-corley-cs-resume.pdf';
 import SF_SKYLINE from '../assets/images/sf-skyline.jpg';
 
-
 // Offset all anchors by -60 to account for a fixed header
 // and scroll more slowly than the default 400ms
 configureAnchors({ offset: -100, scrollDuration: 600 });
@@ -36,21 +35,21 @@ const EMAIL_NATE = 'mailto:nscorley@gmail.com?Subject=Hello%20Nate!';
 export default class App extends React.Component {
   state = { selected: 'hello' };
 
-  setSelected = selected => this.setState({ selected })
-  hideFixedMenu = () => this.setState({ visible: false, selected: 'hello' })
-  showFixedMenu = () => this.setState({ visible: true, selected: 'about' })
+  setSelected = selected => this.setState({ selected });
+  hideFixedMenu = () => this.setState({ visible: false, selected: 'hello' });
+  showFixedMenu = () => this.setState({ visible: true, selected: 'about' });
   handleMenuClick = (e, { name }) => this.setState({ selected: name });
 
   render() {
     const { visible } = this.state;
     return (
       <div>
-        { visible ?
+        {visible ? (
           <FixedMenu
             handleMenuClick={this.handleMenuClick}
             selected={this.state.selected}
           />
-          : null }
+        ) : null}
         <Visibility
           onBottomPassed={this.showFixedMenu}
           onBottomVisible={this.hideFixedMenu}
@@ -79,12 +78,22 @@ export default class App extends React.Component {
               </Grid.Row>
               <Grid.Row>
                 <Container text textAlign="center">
-                  <Image inline shape="circular" size="medium" src={HEADSHOT_SRC} className="header-image" />
+                  <Image
+                    inline
+                    shape="circular"
+                    size="medium"
+                    src={HEADSHOT_SRC}
+                    className="header-image"
+                  />
                   <Header
                     as="h1"
                     content="Hi, I'm Nate."
                     inverted
-                    style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0 }}
+                    style={{
+                      fontSize: '4em',
+                      fontWeight: 'normal',
+                      marginBottom: 0,
+                    }}
                   />
                   <Header
                     as="h2"
@@ -92,7 +101,13 @@ export default class App extends React.Component {
                     inverted
                     style={{ fontSize: '1.7em', fontWeight: 'normal' }}
                   />
-                  <Button as="a" href="#about" onClick={() => this.setSelected('about')} primary size="huge">
+                  <Button
+                    as="a"
+                    href="#about"
+                    onClick={() => this.setSelected('about')}
+                    primary
+                    size="huge"
+                  >
                     Hi Nate! Tell me more!
                     <Icon name="right arrow" />
                   </Button>
@@ -106,8 +121,12 @@ export default class App extends React.Component {
           once={false}
           continuous
           onPassed={{
-            '30%': () => { this.setSelected('about'); },
-            '90%': () => { this.setSelected('work'); },
+            '30%': () => {
+              this.setSelected('about');
+            },
+            '90%': () => {
+              this.setSelected('work');
+            },
           }}
         >
           <Segment style={{ padding: '8em 0em' }} vertical>
@@ -120,12 +139,13 @@ export default class App extends React.Component {
                     </ScrollableAnchor>
                   </Header>
                   <p style={{ fontSize: '1.33em' }}>
-                 Recently, I&#39;ve been building websites with ReactJS and Django.
-                 In the past, I&#39;ve worked on everything
-                 from self-learning chess engines to algorithmic stock trading systems.
+                    Recently, I&#39;ve been building websites with ReactJS and
+                    Django. In the past, I&#39;ve worked on everything from
+                    self-learning chess engines to algorithmic stock trading
+                    systems.
                   </p>
                 </Grid.Column>
-                <Grid.Column textAlign="center" >
+                <Grid.Column textAlign="center">
                   <Icon
                     circular
                     onClick={() => window.open('https://github.com/nscorley')}
@@ -137,12 +157,19 @@ export default class App extends React.Component {
               </Grid.Row>
               <Grid.Row columns={2}>
                 <Grid.Column>
-                  <Header as="h3" style={{ fontSize: '2em' }}>I (occassionally) go outside.</Header>
+                  <Header as="h3" style={{ fontSize: '2em' }}>
+                    I (occassionally) go outside.
+                  </Header>
                   <p style={{ fontSize: '1.33em' }}>
-                 When I&#39;m not tucked behind my monitors, you can find me cycling,
-                 running, or backpacking. Feel free to stalk me on&nbsp;
-                    <a target="_blank" rel="noopener noreferrer" href="https://www.strava.com/athletes/5228967">
-                  Strava
+                    When I&#39;m not tucked behind my monitors, you can find me
+                    cycling, running, or backpacking. Feel free to stalk me
+                    on&nbsp;
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://www.strava.com/athletes/5228967"
+                    >
+                      Strava
                     </a>.
                   </p>
                 </Grid.Column>
@@ -160,10 +187,13 @@ export default class App extends React.Component {
               </Grid.Row>
               <Grid.Row columns={2}>
                 <Grid.Column>
-                  <Header as="h3" style={{ fontSize: '2em' }}>I go to school.</Header>
+                  <Header as="h3" style={{ fontSize: '2em' }}>
+                    I go to school.
+                  </Header>
                   <p style={{ fontSize: '1.33em' }}>
-                 Yup, I&#39;m still slaving away, learning abstract concepts that likely will
-                 bear no influence on my life after graduation.
+                    Yup, I&#39;m still slaving away, learning abstract concepts
+                    that likely will bear no influence on my life after
+                    graduation.
                   </p>
                 </Grid.Column>
                 <Grid.Column textAlign="center">
@@ -185,14 +215,19 @@ export default class App extends React.Component {
         <Visibility
           once={false}
           continuous
-          onPassed={{ '50%': () => { this.setSelected('contact'); } }}
+          onPassed={{
+            '50%': () => {
+              this.setSelected('contact');
+            },
+          }}
         >
           <Segment vertical style={{ padding: '0em' }}>
             <Grid celled stackable>
               <Grid.Row textAlign="center" columns="equal">
-                <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+                <Grid.Column
+                  style={{ paddingBottom: '5em', paddingTop: '5em' }}
+                >
                   <ScrollableAnchor id="work">
-
                     <div>
                       <Image
                         as="a"
@@ -203,21 +238,23 @@ export default class App extends React.Component {
                         src={MODSY_SRC}
                       />
                       <p style={{ fontSize: '1.33em' }}>
-                 I contracted with&nbsp;
+                        I contracted with&nbsp;
                         <a
                           href="http://www.modsy.com"
                           target="_blank"
                           rel="noopener noreferrer"
                         >
-                   Modsy
-                        </a> - a modern, 3D
-                 approach to home redesign - to provide site maintenace and
-                 build internal tooling.
+                          Modsy
+                        </a>{' '}
+                        - a modern, 3D approach to home redesign - to provide
+                        site maintenace and build internal tooling.
                       </p>
                     </div>
                   </ScrollableAnchor>
                 </Grid.Column>
-                <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+                <Grid.Column
+                  style={{ paddingBottom: '5em', paddingTop: '5em' }}
+                >
                   <Image
                     as="a"
                     target="_blank"
@@ -227,13 +264,16 @@ export default class App extends React.Component {
                     src={CAMP_CELIAC_SRC}
                   />
                   <p style={{ fontSize: '1.33em' }}>
-                     Since 2011, I have been the active webmaster for Camp Celiac,
-                     a non-profit, entirely volunteer camp serving children with life-long disabilities.
+                    Since 2011, I have been the active webmaster for Camp
+                    Celiac, a non-profit, entirely volunteer camp serving
+                    children with life-long disabilities.
                   </p>
                 </Grid.Column>
               </Grid.Row>
               <Grid.Row textAlign="center" columns="equal">
-                <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+                <Grid.Column
+                  style={{ paddingBottom: '5em', paddingTop: '5em' }}
+                >
                   <Image
                     as="a"
                     target="_blank"
@@ -243,31 +283,34 @@ export default class App extends React.Component {
                     src={OPTIVER_SRC}
                   />
                   <p style={{ fontSize: '1.33em' }}>
-                 I&#39;m the president of my college&#39;s Algorithmic Trading Club -
-                 a student-run group that attends local competions (we won the
-                 Optiver Marketmaking Competition in 2017) and develops trading strategies.
+                    I&#39;m the president of my college&#39;s Algorithmic
+                    Trading Club - a student-run group that attends local
+                    competions (we won the Optiver Marketmaking Competition in
+                    2017) and develops quantitative trading strategies.
                   </p>
                 </Grid.Column>
-                <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+                <Grid.Column
+                  style={{ paddingBottom: '5em', paddingTop: '5em' }}
+                >
                   <Image
                     as="a"
                     target="_blank"
                     rel="noopener noreferrer"
-                    href="https://github.com/uhsprogrammingclub/Seanet"
+                    href="https://github.com/nscorley/morty-chess"
                     className="chess-icon"
                     src={CHESS_SRC}
                   />
                   <p style={{ fontSize: '1.33em' }}>
-                 I led a team to create&nbsp;
+                    I led a team to create&nbsp;
                     <a
-                      href="https://github.com/uhsprogrammingclub/Seanet"
+                      href="https://github.com/nscorley/morty-chess"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                   SeaNet
-                    </a>
-                 - a computer chess engine that achieved a grandmaster
-                 ELO rating, and became one of the strongest C++ engines.
+                      MortyChess
+                    </a>&nbsp; - a computer chess engine that achieved a
+                    grandmaster ELO rating, and became one of the strongest C++
+                    engines.
                   </p>
                 </Grid.Column>
               </Grid.Row>
@@ -278,30 +321,31 @@ export default class App extends React.Component {
         <Visibility
           once={false}
           continuous
-          onPassed={{ '10%': () => { this.setSelected('work'); } }}
+          onPassed={{
+            '10%': () => {
+              this.setSelected('work');
+            },
+          }}
         >
-          <Segment style={{ padding: '8em 0em', borderBottom: 'none' }} vertical>
+          <Segment
+            style={{ padding: '8em 0em', borderBottom: 'none' }}
+            vertical
+          >
             <Container text>
               <Header as="h3" style={{ fontSize: '2em' }}>
                 <ScrollableAnchor id="contact">
                   <span>This Nate guy seems cool. How can I learn more?</span>
                 </ScrollableAnchor>
-
               </Header>
               <p style={{ fontSize: '1.33em' }}>
-              Glad you think so. Go ahead and check out my&nbsp;
-                <a
-                  target="_blank"
-                  href={RESUME}
-                >
-                resume
+                Glad you think so. Go ahead and check out my&nbsp;
+                <a target="_blank" href={RESUME}>
+                  resume
                 </a>, and please&nbsp;
-                <a
-                  href={EMAIL_NATE}
-                  rel="noopener noreferrer"
-                >
-                reach out
-                </a> to me with any further questions.
+                <a href={EMAIL_NATE} rel="noopener noreferrer">
+                  reach out
+                </a>{' '}
+                to me with any further questions.
               </p>
               <Divider
                 as="h4"
@@ -316,14 +360,10 @@ export default class App extends React.Component {
                   href={RESUME}
                   rel="noopener noreferrer"
                 >
-               Resume
+                  Resume
                 </Button>
-                <Button
-                  as="a"
-                  size="large"
-                  href={EMAIL_NATE}
-                >
-               Email Nate
+                <Button as="a" size="large" href={EMAIL_NATE}>
+                  Email Nate
                 </Button>
               </Divider>
             </Container>
