8bf1f2ec0aca5d9510e7e20e381807745c9d6177
Nathaniel Corley <nscorley@gmail.com>
2018-12-03 20:59:56 +0100

    Please god let this work if you're out there

---
+++ build/precache-manifest.9c733121550a532a90dddd2ba88c9fdc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "276f49ed1a53e01bafbe",
+    "url": "/static/css/main.9bcf29b9.chunk.css"
+  },
+  {
+    "revision": "276f49ed1a53e01bafbe",
+    "url": "/static/js/main.276f49ed.chunk.js"
+  },
+  {
+    "revision": "0ad5d3f47a2bf531faa8",
+    "url": "/static/js/1.0ad5d3f4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1049c33fc96fe3c019e8b75a5656057",
+    "url": "/static/media/dali-portrait.f1049c33.png"
+  },
+  {
+    "revision": "194a78456694684bd00f7b4a610f37e4",
+    "url": "/static/media/botticelli-portrait.194a7845.png"
+  },
+  {
+    "revision": "978c41c19de39ef3cdb3cdebf9462f8d",
+    "url": "/static/media/botticelli-canto-1.978c41c1.jpg"
+  },
+  {
+    "revision": "b4402bac4c9fbc7f569181b80a52467f",
+    "url": "/static/media/dali-canto-1.b4402bac.jpg"
+  },
+  {
+    "revision": "7db4eb5f6cfd9240061aee98956ffbf7",
+    "url": "/static/media/botticelli-canto-10.7db4eb5f.jpg"
+  },
+  {
+    "revision": "f78f459b3dbe3a30e5768ad84342c3eb",
+    "url": "/static/media/dali-canto-10.f78f459b.jpg"
+  },
+  {
+    "revision": "94d04774c8229e604fa91b3c34900890",
+    "url": "/static/media/botticelli-canto-32.94d04774.jpg"
+  },
+  {
+    "revision": "b46ed4e4700bdbe5773c95abac13bc42",
+    "url": "/static/media/dali-canto-32.b46ed4e4.jpg"
+  },
+  {
+    "revision": "c365943e68c6e59ccb33353c480112fa",
+    "url": "/index.html"
+  }
+];
