a6e997f4ceb41262973954d911be4545ad9851fe
Nathaniel Corley <nscorley@gmail.com>
2020-09-30 23:12:16 -0700

    Add files via upload

---
+++ build/crosswords/precache-manifest.17c308d7fab4d762569d352d7b89455c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "795ef447a15be3ca32f7",
+    "url": "/static/js/main.795ef447.chunk.js"
+  },
+  {
+    "revision": "c8333cfd098d0c3d62d5",
+    "url": "/static/js/1.c8333cfd.chunk.js"
+  },
+  {
+    "revision": "795ef447a15be3ca32f7",
+    "url": "/static/css/main.cadf0013.chunk.css"
+  },
+  {
+    "revision": "c8333cfd098d0c3d62d5",
+    "url": "/static/css/1.d0e4647a.chunk.css"
+  },
+  {
+    "revision": "c8e08746174f4fc9fca697b7862de9c3",
+    "url": "/index.html"
+  }
+];
